c59969d2ad898a38e4e4ad41f1b5d72d699d3116
Daniel Dietrich <daniel.dietrich@gmx.net>
2016-01-24 20:46:35 +0100

    Increased readability of tuple examples

--- src/test/java/javaslang/TupleDemo.java
+++ src/test/java/javaslang/TupleDemo.java
@@ -5,7 +5,6 @@
  */
 package javaslang;
 
-
 import org.junit.Test;
 
 import static org.assertj.core.api.BDDAssertions.then;
@@ -13,56 +12,62 @@ import static org.assertj.core.api.BDDAssertions.then;
 public class TupleDemo {
 
     @Test
-    public void createTuple(){
+    public void createTuple() {
         // tag::createTuple[]
         // (Java, 8)
-        Tuple2<String, Integer> java8tuple = Tuple.of("Java", 8); //<1>
+        Tuple2<String, Integer> java8 = Tuple.of("Java", 8); //<1>
 
-        then(java8tuple._1).isEqualTo("Java"); //<2>
-        then(java8tuple._2).isEqualTo(8); //<3>
+        // "Java"
+        String s = java8._1; //<2>
+
+        // 8
+        Integer i = java8._2; //<3>
         // end::createTuple[]
+        then(s).isEqualTo("Java");
+        then(i).isEqualTo(8);
     }
 
     @Test
-    public void bimapTuple(){
-
+    public void bimapTuple() {
+        Tuple2<String, Integer> java8 = Tuple.of("Java", 8);
         // tag::bimapTuple[]
-        // (Java, 8)
-        Tuple2<String, Integer> javaslang2tuple = Tuple.of("Java", 8).map(
-                (s) -> s + "slang", (i) -> i/4
-        );
         // (Javaslang, 2)
-        then(javaslang2tuple._1).isEqualTo("Javaslang");
-        then(javaslang2tuple._2).isEqualTo(2);
+        Tuple2<String, Integer> that = java8.map(
+                s -> s + "slang",
+                i -> i / 4
+        );
         // end::bimapTuple[]
+        then(that._1).isEqualTo("Javaslang");
+        then(that._2).isEqualTo(2);
     }
 
     @Test
-    public void mapTuple(){
-
+    public void mapTuple() {
+        Tuple2<String, Integer> java8 = Tuple.of("Java", 8);
         // tag::mapTuple[]
-        // (Java, 8)
-        Tuple2<String, Integer> javaslang2tuple = Tuple.of("Java", 8).map(
+        // (Javaslang, 2)
+        Tuple2<String, Integer> that = java8.map(
                 (s, i) -> Tuple.of(s + "slang", i / 4)
         );
-        // (Javaslang, 2)
-        then(javaslang2tuple._1).isEqualTo("Javaslang");
-        then(javaslang2tuple._2).isEqualTo(2);
         // end::mapTuple[]
+        then(that._1).isEqualTo("Javaslang");
+        then(that._2).isEqualTo(2);
     }
 
     @Test
-    public void transformTuple(){
+    public void transformTuple() {
+        Tuple2<String, Integer> java8 = Tuple.of("Java", 8);
         // tag::transformTuple[]
-        String javaslang2 = Tuple.of("Java", 8).transform(
-                (s, i) -> s + "slang " + i/4
+        // "Javaslang 2"
+        String that = java8.transform(
+                (s, i) -> s + "slang " + i / 4
         );
-        then(javaslang2).isEqualTo("Javaslang 2");
         // end::transformTuple[]
+        then(that).isEqualTo("Javaslang 2");
     }
 
     @Test
-    public void createEmptyTuple(){
+    public void createEmptyTuple() {
         Tuple0 emptyTuple = Tuple.empty();
     }
 
